eed920dec42d3da08ed1511fa66c0377403abdb9
Robinson1906 <robinsonleonmoreno213@gmail.com>
2023-08-15 19:03:29 -0500

    variables y metodos

---
+++ 04_metodo_post.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Digite la siguiente info</h1>
+    <form action="04_metodo_post.php" method="post">
+        nombre :
+        <input type="text" name="nombre"/>
+        <br>
+        fecha:
+        <input type="date" name="fecha" id=""/>
+        <br>
+        observacion:
+        <input type="text" name="obs"/>
+        <br>
+        <input type="submit" name="enviar"/>
+   
+     
+    </form>
+    <h2>Informacion Envida es: </h2>
+    <?php
+    print_r($_POST);
+?>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,32 @@
+<?php
+//Tipos de Variables
+$nombre = "Fabian";
+$edad = 24;
+$double = 25.6;
+$boolean = false;
+$nombreCurso = "backend";
+
+//Concatenacion
+$concatenacion = '<br>'. $edad . '<br>'.$edad . '<br>'.$double.'<br>';
+echo $concatenacion;
+
+
+echo $nombre;
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>primer programa</h1>
+    <?php
+    echo "hola mundo!";
+    ?>
+</body>
+</html>
